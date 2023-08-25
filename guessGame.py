73500825d4a52c58020afb3d4c026ae3ff7a2ac9
noman081 <abdullahalnoman642@gmail.com>
2023-08-25 18:46:35 +0930

    After adding the checkGuess function

--- guessGame.py
+++ guessGame.py
@@ -8,6 +8,17 @@ class guessGame(object):
     def generate_random_number(self):
         return random.randint(1000, 9999)
 
+    def check_guess(self, secret_number, guess):
+        hints = []
+        for i in range(4):
+            if guess[i] == secret_number[i]:
+                hints.append('o')
+            elif guess[i] in secret_number:
+                hints.append('x')
+            else:
+                hints.append('_')  # Placeholder for digits that don't match
+        print(type(secret_number), type(guess), type(hints))
+        return hints
     
     def play_game(self):
         self.show_detail()
@@ -22,3 +33,12 @@ class guessGame(object):
                 print("Please enter a valid 4-digit number (Press q to quit).")
                 continue
             guess = [int(digit) for digit in guess]
+            hints = self.check_guess(secret_number, guess)
+            if hints == ['o', 'o', 'o', 'o']:
+                print(f"Congratulations! You guessed the number in {attempts} attempts.")
+                play_again = input("Do you want to play again? (y for play again): ").lower()
+                if play_again.lower() == "y":
+                    self.play_game()
+                else:
+                    break
+            attempts += 1
